b6e04247bc6549987a6da0bf9cd471412572cc41
Guilherme Ferreira <106194282+guixande0905g@users.noreply.github.com>
2024-03-04 12:29:21 -0300

    Add files via upload

--- sla.js
+++ sla.js
@@ -1,1 +1,10 @@
 imprime = (msg) => console.log(msg);
+
+FazerSorteio = () => {
+     let num1 = document.getElementById("inicio").value;
+     let num2 = document.getElementById("fim").value;
+     let sorteio = Math.floor( Math.random(inicio,fim+1)*100);
+
+     document.getElementById("resultado").innerHTML = sorteio;
+
+}

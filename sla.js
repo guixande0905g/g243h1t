16ff50d9fd04c47907dbac1a9ac11849bf30d8f9
Guilherme Ferreira <106194282+guixande0905g@users.noreply.github.com>
2024-03-11 12:28:07 -0300

    Add files via upload

--- sla.js
+++ sla.js
@@ -8,3 +8,30 @@ FazerSorteio = () => {
      document.getElementById("resultado").innerHTML = sorteio;
 
 }
+
+function escolher_aba(btn){
+     for(let i = 1; i <= 4; i++) {
+          let id_btn = "btn"+i;
+          let id_aba = "aba"+i;
+          document.getElementById(id_btn).style.backgroundColor = "blue";
+          document.getElementById(id_btn).disabled = false;
+          document.getElementsById(id_aba).style.display = "none"
+     }
+     if(btn == "1"){
+     document.getElementById("btn1").style.backgroundColor = "red";
+     document.getElementById("btn1").disabled = true;
+     document.getElementById("ab1")
+    }
+     if(btn == "2"){
+     document.getElementById("btn2").style.backgroundColor = "red";
+     document.getElementById("btn2").disabled = true;
+}
+     if(btn == "3"){
+     document.getElementById("btn3").style.backgroundColor = "red";
+     document.getElementById("btn3").disabled = true;
+}
+     if(btn == "4"){
+     document.getElementById("btn4").style.backgroundColor = "red";
+     document.getElementById("btn4").disabled = true;
+}
+}
